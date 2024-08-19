736a52bd57c701bef533676ac84ee782a8ac8575
vinhpt-0479 <pham.thanh.vinh@sun-asterisk.com>
2024-08-19 11:14:49 +0700

    feat(order): change order status with customer role and logout feature

--- src/main/java/group1/intern/service/OrdersService.java
+++ src/main/java/group1/intern/service/OrdersService.java
@@ -1,6 +1,7 @@
 package group1.intern.service;
 
 import group1.intern.bean.OrderInfo;
+import group1.intern.model.Account;
 import group1.intern.model.Enum.OrderStatus;
 import jakarta.annotation.Nullable;
 import org.springframework.data.domain.Page;
@@ -8,4 +9,6 @@ import org.springframework.data.domain.Pageable;
 
 public interface OrdersService {
     Page<OrderInfo> getOrders(@Nullable Integer accountId, @Nullable OrderStatus status, Pageable pageable);
+
+    void changeOrderStatus(Account account, Integer orderId, String status);
 }
