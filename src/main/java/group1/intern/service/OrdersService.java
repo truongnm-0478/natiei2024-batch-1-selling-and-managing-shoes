8ee54aa1e9c20fccb8031e5a5bd448daacf7f0da
ngtueminh <nguyentueminh0305@gmail.com>
2024-08-23 16:06:19 +0700

    [Role Admin] [Statiѕtic Screen] Theo ngày, tháng, năm, khoảng thời gian

--- src/main/java/group1/intern/service/OrdersService.java
+++ src/main/java/group1/intern/service/OrdersService.java
@@ -1,11 +1,15 @@
 package group1.intern.service;
 
 import group1.intern.bean.OrderInfo;
+import group1.intern.bean.RevenueInfo;
 import group1.intern.model.Account;
 import group1.intern.model.Enum.OrderStatus;
 import jakarta.annotation.Nullable;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,4 +21,7 @@ public interface OrdersService {
     long countByDate(LocalDate date);
     double sumTotalPriceByDate(LocalDate date);
     double sumEstimatedRevenueByDate(LocalDate date);
+    RevenueInfo getMonthlyRevenue(YearMonth yearMonth);
+    Map<String, RevenueInfo> getDailyRevenueDetails(YearMonth yearMonth);
+    Map<String, RevenueInfo> getMonthlyRevenueDetails(int year);
 }
